3aabd386995e7d354939769100adfd6e9a81b51f
josiec09 <itzjmo@hotmail.com>
2019-12-04 16:00:04 -0600

    Delete mycog.py

--- mycog.py
+++
@@ -1,9 +0,0 @@
-from redbot.core import commands
-
-class Mycog(commands.Cog):
-    """My custom cog"""
-
-    @client.event
-	async def mycom(message):
-    if message.content.startswith("#whatever you want it to be")
-        await client.send_message(message.author, "#The message")
